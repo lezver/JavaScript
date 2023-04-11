ed5c584c7cd16de77775d2f6f794c39a3c30c666
Vladislav <lezver1994@gmail.com>
2023-04-11 15:41:23 +0300

    practice

---
+++ module9/notification/index.js
@@ -0,0 +1,24 @@
+const refs = {
+	btn: document.querySelector(".btn"),
+	info: document.querySelector(".notification-info"),
+};
+
+const NOTIFICATION_DELAY = 5000;
+let timeOutId = null;
+
+const showNotification = () => {
+	refs.info.classList.add("is-visible");
+	timeOutId = setTimeout(() => {
+		console.log("not work");
+		hideNotification();
+	}, NOTIFICATION_DELAY);
+};
+
+const hideNotification = () => {
+	refs.info.classList.remove("is-visible");
+	clearTimeout(timeOutId);
+};
+
+refs.btn.addEventListener("click", showNotification);
+
+refs.info.addEventListener("click", hideNotification);
